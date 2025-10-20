3345980c95a180a908ec1ee0e23b626088713baf
Jazx Mok <weije2009@gmail.com>
2025-10-20 14:28:28 +0800

    initial commit

---
+++ Environment-variables/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 8080;
+
+app.get('/',(req,res)=>{
+    res.send('Hello from express')
+})
+
+app.listen(port, () => {
+    console.log('Server Listening on port ${port}')
+})
